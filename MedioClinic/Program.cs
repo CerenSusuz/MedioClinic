350ee7f8ed1e2fdbc8aefe90aff96bc105645897
Ceren Susuz <ceren199704@hotmail.com>
2022-02-17 12:49:14 +0300

    add Logging process

--- MedioClinic/Program.cs
+++ MedioClinic/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using XperienceAdapter.Logging;
 
 namespace MedioClinic
 {
@@ -18,9 +19,17 @@ namespace MedioClinic
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                // a call to the AddXperience method you've just implemented.
+                .ConfigureLogging(logging => logging.AddXperience())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
     }
+
+    //In this example, the logging infrastructure is invoked in the Main method, according to the best practice outlined by Microsoft. 
+    //Experience logger relies on a service, whose object is constructed by the Experience built-in DI container.
+    //During start, that container becomes functional only after the core Experience objects have been built out of database data.
+    //This means that if your database server happens to have slower initial connection times, the logger may start failing at times, resulting in exception messages at startup.
+    //In such case, we recommend invoking your logger after AddKentico() is run in Startup.ConfigureServices.
 }
---
+++ XperienceAdapter/Logging/XperienceLoggerExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XperienceAdapter.Logging
+{
+    /// <summary>
+    /// the class static so that it can hold extension methods.
+    /// </summary>
+    public static class XperienceLoggerExtensions
+    {
+        /// <summary>
+        /// AddXperience method that later call at the application start.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static ILoggingBuilder AddXperience(this ILoggingBuilder builder)
+        {
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, XperienceLoggerProvider>());
+
+            return builder;
+        }
+
+        /// <summary>
+        ///  a convenience LogEvent method that creates an event name from the name of the client method.
+        ///  If the methodName argument is not available, the log entry will simply contain less information.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="logLevel"></param>
+        /// <param name="methodName"></param>
+        /// <param name="message"></param>
+        /// <param name="exception"></param>
+        /// <param name="args"></param>
+        public static void LogEvent(this ILogger logger, LogLevel logLevel, string? methodName, string? message = default, Exception? exception = default, params object[] args)
+        {
+            if (!string.IsNullOrEmpty(methodName))
+            {
+                var eventId = new EventId(0, methodName);
+
+                logger.Log(logLevel, eventId, exception, message, args);
+            }
+            else
+            {
+                logger.Log(logLevel, exception, message, args);
+            }
+        }
+
+    }
+}
