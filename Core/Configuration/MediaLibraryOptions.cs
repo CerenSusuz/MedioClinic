b12a031ca63f28ef1a249b982957562a84459801
Ceren Susuz <ceren199704@hotmail.com>
2022-02-24 20:18:37 +0300

    Create the IOptions configuration class

---
+++ Core/Configuration/MediaLibraryOptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Configuration
+{
+
+	/// <summary>
+	/// Media library options.
+	/// </summary>
+	public class MediaLibraryOptions
+	{
+		/// <summary>
+		/// Image formats allowed in the site.
+		/// </summary>
+		public string[]? AllowedImageExtensions { get; set; }
+
+		/// <summary>
+		/// File size limit.
+		/// </summary>
+		public long FileSizeLimit { get; set; }
+	}
+}
