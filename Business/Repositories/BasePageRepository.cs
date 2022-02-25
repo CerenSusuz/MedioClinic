bd99f304f5b91e9b76f4687d1c077de63fc7f0fc
Ceren Susuz <ceren199704@hotmail.com>
2022-02-25 15:11:09 +0300

    Create the non-abstract base page repository.

---
+++ Business/Repositories/BasePageRepository.cs
@@ -0,0 +1,21 @@
+﻿using CMS.DocumentEngine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using XperienceAdapter.Models;
+using XperienceAdapter.Repositories;
+using XperienceAdapter.Services;
+
+namespace Business.Repositories
+{
+	/// <summary>
+	/// Stores pages without page type-specific data (coupled data).
+	///  this class only compensates for the abstract nature of XperienceAdapter.Repositories.BasePageRepository.
+	/// </summary>
+	public class BasePageRepository : BasePageRepository<BasicPage, TreeNode>
+	{
+		public BasePageRepository(IRepositoryServices repositoryDependencies) : base(repositoryDependencies)
+		{
+		}
+	}
+}
