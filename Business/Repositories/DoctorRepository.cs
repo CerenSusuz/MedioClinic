c853f787b0bcf91670cd91ad70c31dbffe856f3c
Ceren Susuz <ceren199704@hotmail.com>
2022-02-25 13:44:18 +0300

    Create a backbone of the doctor repository class.

---
+++ Business/Repositories/DoctorRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+using CMS.DocumentEngine;
+using CMS.Membership;
+
+using XperienceAdapter.Extensions;
+using XperienceAdapter.Repositories;
+using XperienceAdapter.Services;
+using Business.Extensions;
+using Business.Models;
+
+
+namespace Business.Repositories
+{
+	public class DoctorRepository : BasePageRepository<Doctor, CMS.DocumentEngine.Types.MedioClinic.Doctor>
+	{
+		private readonly IUserInfoProvider _userInfoProvider;
+
+		public DoctorRepository(IRepositoryServices repositoryServices, IUserInfoProvider userInfoProvider) : base(repositoryServices)
+		{
+			_userInfoProvider = userInfoProvider ?? throw new ArgumentNullException(nameof(userInfoProvider));
+		}
+
+		private static DayOfWeek? GetShiftDayOfWeek(IEnumerable<TreeNode> dayOfWeekPage) =>
+			dayOfWeekPage.ToDaysOfWeek().FirstOrDefault();
+	}
+}
