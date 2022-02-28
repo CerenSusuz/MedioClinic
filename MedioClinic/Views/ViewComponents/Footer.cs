b782ea834081423fb2e81d376ade00e5767c6acc
Ceren Susuz <ceren199704@hotmail.com>
2022-02-28 20:02:33 +0300

    Creating a footer view component

---
+++ MedioClinic/Views/ViewComponents/Footer.cs
@@ -0,0 +1,9 @@
+﻿#define no_suffix
+
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+public class Footer : ViewComponent
+{
+	public async Task<IViewComponentResult> InvokeAsync() => View();
+}
