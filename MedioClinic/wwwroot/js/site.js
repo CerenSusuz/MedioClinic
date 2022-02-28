62d5a8bc203e39cb5fc357fa48aa78f896d3d10f
Ceren Susuz <ceren199704@hotmail.com>
2022-02-28 18:37:35 +0300

    Register the JavaScript functionality

--- MedioClinic/wwwroot/js/site.js
+++ MedioClinic/wwwroot/js/site.js
@@ -1,5 +1,8 @@
 ﻿
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
 
+// Write your JavaScript code.
 //Use the DOMContentLoaded event to run code after the DOM is fully loaded.
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -10,4 +13,9 @@ document.addEventListener("DOMContentLoaded", function () {
     M.Dropdown.init(dropdownElements, {
         hover: false
     });
+
+    var selectElements = document.querySelectorAll("select");
+    M.FormSelect.init(selectElements);
+    var datepickerElements = document.querySelectorAll("datepicker");
+    M.Datepicker.init(datepickerElements);
 });
