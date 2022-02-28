4c585dfc4410a90b91a3a3141791210c4b71207d
Ceren Susuz <ceren199704@hotmail.com>
2022-02-28 13:53:13 +0300

    Create the user message class

---
+++ MedioClinic/Models/UserMessage.cs
@@ -0,0 +1,23 @@
+﻿namespace MedioClinic.Models
+{
+	/// <summary>
+	/// This class captures toast notifications to the site visitors.
+	/// </summary>
+	public class UserMessage
+	{
+		public MessageType MessageType { get; set; }
+
+		public string? Message { get; set; }
+
+		public bool DisplayAsRaw { get; set; }
+
+		public bool Display { get; set; }
+	}
+
+	public enum MessageType
+	{
+		Info,
+		Warning,
+		Error
+	}
+}
