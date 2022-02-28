3b5685168ce2a3dfc01c7a1bc77e7f0619e05234
Ceren Susuz <ceren199704@hotmail.com>
2022-02-28 14:06:03 +0300

    Create the site-wide JavaScript file

---
+++ MedioClinic/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿
+
+//Use the DOMContentLoaded event to run code after the DOM is fully loaded.
+
+document.addEventListener("DOMContentLoaded", function () {
+    var sidenavElements = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenavElements);
+    var dropdownElements = document.querySelectorAll(".dropdown-trigger");
+
+    M.Dropdown.init(dropdownElements, {
+        hover: false
+    });
+});
