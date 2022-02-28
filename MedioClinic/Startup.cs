b5df19ce5c8e473fca20b584f81299314b6f5c31
Ceren Susuz <ceren199704@hotmail.com>
2022-02-28 13:49:52 +0300

    Implement a universal exception handler

--- MedioClinic/Startup.cs
+++ MedioClinic/Startup.cs
@@ -92,6 +92,34 @@ namespace MedioClinic
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
             }
+            //handle the most low-level errors in the app.
+            else
+            {
+                app.UseExceptionHandler(errorApp =>
+                {
+                    errorApp.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        context.Response.ContentType = "text/html";
+
+                        await context.Response.WriteAsync("<html lang=\"en\"><body>\r\n");
+                        await context.Response.WriteAsync("An error happened.<br><br>\r\n");
+
+                        var exceptionHandlerPathFeature =
+                            context.Features.Get<Microsoft.AspNetCore.Diagnostics.IExceptionHandlerPathFeature>();
+
+                        if (exceptionHandlerPathFeature?.Error is System.IO.FileNotFoundException)
+                        {
+                            await context.Response.WriteAsync("A file error happened.<br><br>\r\n");
+                        }
+
+                        await context.Response.WriteAsync("<a href=\"/\">Home</a><br>\r\n");
+                        await context.Response.WriteAsync("</body></html>\r\n");
+                        await context.Response.WriteAsync(new string(' ', 512)); // IE padding
+                    });
+                });
+
+            }
 
             app.UseLocalizedStatusCodePagesWithReExecute("/{0}/error/{1}/");
 
