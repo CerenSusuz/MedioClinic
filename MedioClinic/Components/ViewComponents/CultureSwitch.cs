737f7be602c8fbaca8f5ee6ba76865860faef56e
Ceren Susuz <ceren199704@hotmail.com>
2022-02-28 19:47:36 +0300

    Create the skeleton of the component class

---
+++ MedioClinic/Components/ViewComponents/CultureSwitch.cs
@@ -0,0 +1,27 @@
+﻿#define no_suffix
+
+using Business.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using XperienceAdapter.Repositories;
+
+...
+
+public class CultureSwitch : ViewComponent
+{
+	private readonly string[] ExcludedPaths =
+	{
+		"landing-pages",
+		"paginas-de-destino"
+	};
+
+	private readonly INavigationRepository _navigationRepository;
+
+	private readonly ISiteCultureRepository _siteCultureRepository;
+
+	public CultureSwitch(ISiteCultureRepository siteCultureRepository, INavigationRepository navigationRepository)
+	{
+		_siteCultureRepository = siteCultureRepository ?? throw new ArgumentNullException(nameof(siteCultureRepository));
+		_navigationRepository = navigationRepository ?? throw new ArgumentNullException(nameof(navigationRepository));
+	}
+}
