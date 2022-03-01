e8dfbb844fc1554d634f968d8fbd616bb5202f31
Ceren Susuz <ceren199704@hotmail.com>
2022-03-01 11:49:49 +0300

    Add helper methods

--- MedioClinic/Controllers/BaseController.cs
+++ MedioClinic/Controllers/BaseController.cs
@@ -59,7 +59,61 @@ namespace MedioClinic.Controllers
                 displayAsRaw,
                 messageType);
 
+        //methods that add ancillary features to the controllers in the application. 
 
+        /// <summary>
+        /// Localizes a string resource.
+        /// methods will simplify access to localized resource strings.
+        /// </summary>
+        /// <param name="resourceKey">Resource key.</param>
+        /// <returns></returns>
+        protected string Localize(string resourceKey) => _stringLocalizer[resourceKey];
+
+        /// <summary>
+        /// Localizes a string resource using a pattern.
+        /// </summary>
+        /// <param name="resourceKey">Resource key.</param>
+        /// <param name="args">The values to format the string with.</param>
+        /// <returns></returns>
+        protected string Localize(string resourceKey, params object[] args) => _stringLocalizer[resourceKey, args];
+
+        /// <summary>
+        /// Returns an error message about an invalid user input.
+        /// method will display validation errors in forms.
+        /// </summary>
+        /// <typeparam name="TUploadViewModel">Upload view model.</typeparam>
+        /// <param name="uploadModel">Upload view model.</param>
+        /// <returns>A message view.</returns>
+        protected ActionResult InvalidInput<TUploadViewModel>(
+            PageViewModel<TUploadViewModel> uploadModel)
+            where TUploadViewModel : class, new()
+        {
+            var metadata = new Models.PageMetadata
+            {
+                Title = Localize("General.InvalidInput.Title")
+            };
+
+            var viewModel = GetPageViewModel(
+                metadata,
+                uploadModel.Data,
+                Localize("General.InvalidInput.Message"),
+                true,
+                false,
+                MessageType.Error);
+
+            return View(viewModel);
+        }
+
+        /// <summary>
+        /// Concatenates an error message with a "contact administrator" suffix.
+        /// method will append the "Please contact your administrator" instruction to error messages.
+        /// </summary>
+        /// <param name="messageKey">A resource key of the error message.</param>
+        /// <returns>Concatenated error messages.</returns>
+        protected string ConcatenateContactAdmin(string messageKey) =>
+            Localize(messageKey)
+                + " "
+                + Localize("General.ContactAdministrator");
 
 
 
